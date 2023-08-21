3f25e3d0522e05daea9f805f12a1807fb2f8aef0
ndzy01 <2326962137@qq.com>
2023-08-21 23:57:09 +0800

    chore: 优化逻辑

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { useContext } from 'react';
 import { useMount } from 'ahooks';
-import { Tree, Space, Popconfirm, Spin, Button, Drawer, Form, Input } from 'antd';
+import { Tree, Space, Popconfirm, Spin, Button, Drawer, Form, Input, message } from 'antd';
 import type { DirectoryTreeProps } from 'antd/es/tree';
 import { ReduxContext } from '../redux';
 import { serviceAxios } from '../utils';
@@ -11,7 +11,6 @@ import Preview from './components/Preview';
 
 const { DirectoryTree } = Tree;
 const Home = () => {
-  const [form] = Form.useForm();
   const { state, dispatch } = useContext(ReduxContext);
   const [articles, setArticles] = useState([]);
   const [open, setOpen] = useState(false);
@@ -49,6 +48,11 @@ const Home = () => {
   };
 
   const goEdit = () => {
+    if (!state.article?.id || !state.article?.isLeaf) {
+      message.warning('请选择您要更新的文章');
+      return;
+    }
+
     setOpen(true);
     setEditType('1');
   };
@@ -65,10 +69,6 @@ const Home = () => {
   };
 
   const del = () => {
-    if (!state.article?.id || !state.article?.isLeaf) {
-      return;
-    }
-
     dispatch({ type: 'UPDATE', payload: { loading: true } });
 
     serviceAxios
@@ -90,22 +90,16 @@ const Home = () => {
           pId: state.article?.id ? state.article?.id : '0',
         })
         .finally(() => {
-          form.resetFields();
           setOpen(false);
           getAll();
         });
     } else if (editType === '1') {
-      if (!state.article?.id) {
-        return;
-      }
-
       serviceAxios
         .patch(`/tree/${state.article?.id}`, {
           name: values.name,
           content: values.content,
         })
         .finally(() => {
-          form.resetFields();
           setOpen(false);
           getAll();
         });
@@ -135,9 +129,11 @@ const Home = () => {
               <Button onClick={goCreateRoot}>新增根目录</Button>
               <Button onClick={goCreate}>新增</Button>
               <Button onClick={goEdit}>编辑</Button>
-              <Popconfirm title="删除将无法恢复,确定删除?" onConfirm={del}>
-                <Button>删除</Button>
-              </Popconfirm>
+              {state.article?.id && state.article.isLeaf && (
+                <Popconfirm title="删除将无法恢复,确定删除?" onConfirm={del}>
+                  <Button>删除</Button>
+                </Popconfirm>
+              )}
             </Space>
             <DirectoryTree
               selectedKeys={state.article?.id ? [state.article?.id] : []}
@@ -151,58 +147,91 @@ const Home = () => {
 
             {open && (
               <Drawer
+                destroyOnClose
                 width="100%"
                 title={editType === '0' ? '新增' : '编辑'}
                 placement="right"
                 onClose={onClose}
                 open={open}
               >
-                <Form
-                  initialValues={
-                    editType === '1'
-                      ? {
-                          name: state.article?.name,
-                          content: state.article?.content,
-                        }
-                      : undefined
-                  }
-                  name="update"
-                  onFinish={update}
-                  scrollToFirstError
-                  form={form}
-                >
-                  <Form.Item
-                    name="name"
-                    label="名称"
-                    rules={[
-                      {
-                        required: true,
-                        message: '名称不能为空',
-                      },
-                    ]}
+                {editType === '1' ? (
+                  <Form
+                    preserve={false}
+                    initialValues={{
+                      name: state.article?.name,
+                      content: state.article?.content,
+                    }}
+                    name="update"
+                    onFinish={update}
+                    scrollToFirstError
                   >
-                    <Input.TextArea rows={1} />
-                  </Form.Item>
+                    <Form.Item
+                      name="name"
+                      label="名称"
+                      rules={[
+                        {
+                          required: true,
+                          message: '名称不能为空',
+                        },
+                      ]}
+                    >
+                      <Input.TextArea rows={1} />
+                    </Form.Item>
 
-                  <Form.Item
-                    name="content"
-                    label="内容"
-                    rules={[
-                      {
-                        required: true,
-                        message: '内容不能为空',
-                      },
-                    ]}
-                  >
-                    <Editor />
-                  </Form.Item>
+                    <Form.Item
+                      name="content"
+                      label="内容"
+                      rules={[
+                        {
+                          required: true,
+                          message: '内容不能为空',
+                        },
+                      ]}
+                    >
+                      <Editor />
+                    </Form.Item>
 
-                  <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                      保存
-                    </Button>
-                  </Form.Item>
-                </Form>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        保存
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                ) : (
+                  <Form name="update" onFinish={update} scrollToFirstError preserve={false}>
+                    <Form.Item
+                      name="name"
+                      label="名称"
+                      rules={[
+                        {
+                          required: true,
+                          message: '名称不能为空',
+                        },
+                      ]}
+                    >
+                      <Input.TextArea rows={1} />
+                    </Form.Item>
+
+                    <Form.Item
+                      name="content"
+                      label="内容"
+                      rules={[
+                        {
+                          required: true,
+                          message: '内容不能为空',
+                        },
+                      ]}
+                    >
+                      <Editor />
+                    </Form.Item>
+
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        保存
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                )}
               </Drawer>
             )}
           </>
